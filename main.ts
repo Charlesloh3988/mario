6bee1b43537aa532c791be5fd012351a7c7b3a97
Charlesloh3988 <110405821+Charlesloh3988@users.noreply.github.com>
2022-08-05 00:49:35 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+music.setTempo(165)
+music.playTone(196, music.beat(BeatFraction.Half))
+music.playTone(262, music.beat(BeatFraction.Half))
+music.playTone(330, music.beat(BeatFraction.Half))
+music.playTone(392, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Half))
+music.playTone(659, music.beat(BeatFraction.Half))
+music.playTone(784, music.beat(BeatFraction.Whole))
+music.playTone(659, music.beat(BeatFraction.Whole))
+for (let index = 0; index < 1; index++) {
+    music.playTone(208, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(311, music.beat(BeatFraction.Half))
+    music.playTone(415, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.playTone(622, music.beat(BeatFraction.Half))
+    music.playTone(831, music.beat(BeatFraction.Whole))
+    music.playTone(622, music.beat(BeatFraction.Whole))
+}
+for (let index = 0; index < 1; index++) {
+    music.playTone(233, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(466, music.beat(BeatFraction.Half))
+    music.playTone(587, music.beat(BeatFraction.Half))
+    music.playTone(698, music.beat(BeatFraction.Half))
+    music.playTone(932, music.beat(BeatFraction.Whole))
+    music.playTone(932, music.beat(BeatFraction.Half))
+    music.playTone(932, music.beat(BeatFraction.Half))
+    music.playTone(932, music.beat(BeatFraction.Half))
+    music.playTone(1046, music.beat(BeatFraction.Double))
+}
+basic.forever(function () {
+	
+})
